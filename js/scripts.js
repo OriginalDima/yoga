8551f9a40a32cd2679c5212e861119b5dec5800d
Dmytro <dmytroresler@gmail.com>
2019-09-10 20:18:29 +0200

    add countdown timer

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
     'use strict';
 
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
-    
+
     function hideTabContent(a) {
         for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
@@ -21,7 +21,7 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    info.addEventListener('click', function(event) {
+    info.addEventListener('click', function (event) {
         let target = event.target;
         if (target && target.classList.contains('info-header-tab')) {
             for (let i = 0; i < tab.length; i++) {
@@ -33,4 +33,60 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    // timer
+
+    let deadline = '2019-10-22';
+
+    function getTimeRemining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            // hours = Math.floor((t / (1000 * 60 * 60)));
+            hours = (Math.floor((t / 1000 / 60) % 24)),
+            days = Math.floor((t / (1000 * 60 * 60 * 24)));
+
+
+        return {
+            'total': t,
+            'days' : days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id),
+            days = timer.querySelector('.days'),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemining(endTime);
+
+            function addZero(num) {
+                if (num <= 9) {
+                    return '0' + num;
+                } else return num;
+            };
+
+            days.textContent = addZero(t.days + ' д');
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
